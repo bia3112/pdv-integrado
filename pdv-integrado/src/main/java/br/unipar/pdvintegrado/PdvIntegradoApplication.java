0441b21158dbbc2cbcea78787bcb775c8f810a91
Beatriz <beatriz.omori@edu.unipar.br>
2024-05-27 21:47:48 -0300

    criação projeto

---
+++ pdv-integrado/src/main/java/br/unipar/pdvintegrado/PdvIntegradoApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.pdvintegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdvIntegradoApplication.class, args);
+    }
+
+}
